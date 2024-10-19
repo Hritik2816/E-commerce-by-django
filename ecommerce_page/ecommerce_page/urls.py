a5c1b8acec5aeb9df60fb8085487351d8b40c7d8
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2024-10-20 03:29:53 +0530

    make a project docs and then create a app also set all config

--- ecommerce_page/ecommerce_page/urls.py
+++ ecommerce_page/ecommerce_page/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import views
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about")
+    path('admin/', admin.site.urls),
+    path('ecommerce_product/', include('ecommerce_product.urls'))
 ]
